72218be12ac2110f1a33e52da172faef6290ea7a
Alonso G <71342528+AlonsoMtzG@users.noreply.github.com>
2022-06-10 12:45:36 -0500

    Practice 6

---
+++ practice6.sql
@@ -0,0 +1,32 @@
+-- INDEXES
+
+-- Use Data structure that helps to optimize searching
+
+CREATE TABLE new_york_addresses (
+    longitude numeric(9,6),
+    latitude numeric(9,6),
+    street_number text,
+    street text,
+    unit text,
+    postcode text,
+    id integer CONSTRAINT new_york_key PRIMARY KEY
+);
+
+COPY new_york_addresses
+FROM '/tmp/city_of_new_york.csv'
+WITH (FORMAT CSV, HEADER);
+
+-- Queries for index performance
+
+EXPLAIN ANALYZE SELECT * FROM new_york_addresses
+WHERE street = 'BROADWAY';
+
+EXPLAIN ANALYZE SELECT * FROM new_york_addresses
+WHERE street = '52 STREET';
+
+EXPLAIN ANALYZE SELECT * FROM new_york_addresses
+WHERE street = 'ZWICKY AVENUE';
+
+-- B-tree index
+
+CREATE INDEX street_idx ON new_york_addresses (street);
